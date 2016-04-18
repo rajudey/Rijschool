fd7fa107af095ad37b24969bde2c435cfad28b3d
Raju Dey <raju_grasim@yahoo.com>
2016-04-18 15:31:36 +0000

    edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :destroy ]
   def new
     @user = User.new
   end
@@ -7,11 +8,29 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to Rijschool #{@user.username}"
-      redirect_to schools_path(@user)
+      redirect_to schools_path 
     else
       render 'new'
     end 
   end
+
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = "User was successfully updated"
+      redirect_to schools_path 
+    else
+      render 'edit'
+    end    
+  end
+  
+  def destroy
+     @user.destroy
+     flash[:danger] = "User successfully deleted"
+     redirect_to users_path 
+  end  
   
   private
   def set_user
