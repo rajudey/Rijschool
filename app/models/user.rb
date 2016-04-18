c692af5aab2d685a953cb843bfb033edcafb3102
Raju Dey <raju_grasim@yahoo.com>
2016-04-18 13:01:21 +0000

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}
+  VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,    presence: true, uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX}
+  validates :initials, presence: true
+  validates :fname,    presence: true   
+  validates :lname,    presence: true
+  validates :sex,      presence: true
+  validates :add1,     presence: true
+  validates :city,     presence: true  
+  validates :postcode, presence: true
 end
